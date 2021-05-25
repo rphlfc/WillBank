aa48441a3bda8367b25422dd56fb69ae5c4243da
Raphael Cerqueira <raphaelfc@gmail.com>
2021-05-25 19:32:49 -0300

    Initial Commit

---
+++ Will/WillApp.swift
@@ -0,0 +1,17 @@
+//
+//  WillApp.swift
+//  Will
+//
+//  Created by Raphael Cerqueira on 25/05/21.
+//
+
+import SwiftUI
+
+@main
+struct WillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
