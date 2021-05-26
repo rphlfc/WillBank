b1057980b5e20e7012b21dec312e01ed8a65b9c4
Raphael Cerqueira <raphaelfc@gmail.com>
2021-05-26 05:35:57 -0300

    Primeiro commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Tela principal do aplicativo do Banco Will usando SwiftUI
+Recentemente me cadastrei no [Will Bank](https://www.willbank.com.br) para entender como foi feito o onboarding no aplicativo. Achei interessante implementar a tela principal do aplicativo usando SwiftUI.
+
+## Screenshots
+<p align="center">
+    <img src="https://user-images.githubusercontent.com/16376748/119581657-5e058c80-bd99-11eb-96bd-f01604bcb7b7.png" width="400">
+</p>
+
+## Vídeos
+- [Vídeo no YouTube](https://youtu.be/ToWEjLbnURw)
+
+## Requisitos
+- iOS 14
+- SwiftUI
+- Xcode 12
+
+## Meta
+- Github: [https://github.com/rphlfc](https://github.com/rphlfc)
+- Instagram: [https://www.instagram.com/rphlfc.dev](https://www.instagram.com/rphlfc.dev)
+- LinkedIn: [http://www.linkedin.com/in/rphlfc](http://www.linkedin.com/in/rphlfc)
+- Twitter: [https://twitter.com/rphlfc](https://twitter.com/rphlfc)
+- Website: [https://rphlfc.github.io](https://rphlfc.github.io)
+- Youtube: [https://www.youtube.com/c/raphaelcerqueira](https://www.youtube.com/c/raphaelcerqueira)
--- Will/ContentView.swift
+++ Will/ContentView.swift
@@ -8,9 +8,277 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isVisible = true
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            BackgroundShape()
+                .ignoresSafeArea(.all, edges: .top)
+                .foregroundColor(Color("yellow"))
+            
+            ScrollView(.vertical, showsIndicators: false, content: {
+                VStack(spacing: 20) {
+                    HeaderView(isVisible: $isVisible)
+                        .padding(.horizontal)
+                    
+                    AccountView(isVisible: $isVisible)
+                        .frame(height: 150)
+                        .padding(.horizontal)
+                    
+                    CardView(isVisible: $isVisible)
+                        .padding(20)
+                        .background(Color("cardBackground"))
+                        .cornerRadius(8)
+                        .padding(.horizontal)
+                    
+                    CardStatusView()
+                        .padding(20)
+                        .background(Color("cardBackground"))
+                        .cornerRadius(8)
+                        .padding(.horizontal)
+                    
+                    InviteView()
+                        .padding(20)
+                        .background(Color("cardBackground"))
+                        .cornerRadius(8)
+                        .padding(.horizontal)
+                    
+                    PollView()
+                        .background(Color("cardBackground"))
+                        .cornerRadius(8)
+                        .padding(.horizontal)
+                    
+                    Spacer()
+                }
+            })
+        }
+    }
+}
+
+struct PollView: View {
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading, spacing: 20) {
+                Text("Está curtindo o app?")
+                    .font(.system(size: 21, weight: .bold))
+                
+                Text("Clique aqui para me contar como está sendo sua experiência. Sua opinião é essencial!")
+                    .font(.system(size: 19))
+            }
+            .foregroundColor(Color.black.opacity(0.7))
+            .padding(20)
+            
+            Spacer()
+            
+            VStack {
+                Spacer()
+                
+                Image("image2")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 140)
+            }
+        }
+    }
+}
+
+struct InviteView: View {
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text("Convidar amigos")
+                    .font(.system(size: 21, weight: .bold))
+                
+                Text("Chame todo mundo para ser will também.")
+                    .font(.system(size: 19))
+                    .padding(.top, 40)
+            }
+            .foregroundColor(Color.black.opacity(0.7))
+            
+            Spacer()
+            
+            Image("image1")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+        }
+    }
+}
+
+struct CardStatusView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Gerenciar cartões")
+                    .fontWeight(.bold)
+                    .foregroundColor(.gray)
+                
+                Spacer()
+                
+                Image(systemName: "chevron.right")
+                    .font(.system(size: 17, weight: .bold))
+                    .foregroundColor(.gray)
+            }
+            
+            VStack(spacing: 12) {
+                HStack(spacing: 12) {
+                    Image(systemName: "checkmark.circle.fill")
+                        .foregroundColor(.green)
+                    
+                    Text("Virtual")
+                    
+                    Text("Liberado para compras")
+                        .fontWeight(.bold)
+                    
+                    Spacer()
+                }
+                
+                HStack(spacing: 12) {
+                    Image(systemName: "envelope.fill")
+                        
+                    Text("Físico")
+                    
+                    Text("Acompanhe a entrega")
+                        .fontWeight(.bold)
+                    
+                    Spacer()
+                }
+            }
+            .foregroundColor(.black.opacity(0.7))
+            .padding(.top, 40)
+        }
+    }
+}
+
+struct CardView: View {
+    @Binding var isVisible: Bool
+    
+    var body: some View {
+        HStack {
+            Text("Limite disponível no cartão")
+                .foregroundColor(.gray)
+                .fontWeight(.bold)
+            
+            Spacer()
+            
+            Text(isVisible ? "R$ 2.250,00" : "R$ •••••••••")
+                .font(.system(size: 19))
+                .fontWeight(.bold)
+            
+            Image(systemName: "chevron.right")
+                .font(.system(size: 17, weight: .bold))
+                .foregroundColor(.gray)
+        }
+    }
+}
+
+struct AccountView: View {
+    @Binding var isVisible: Bool
+    
+    var body: some View {
+        HStack(spacing: 15) {
+            Button(action: {}, label: {
+                RoundedRectangle(cornerRadius: 8)
+                    .foregroundColor(.white)
+                    .overlay(
+                        VStack {
+                            HStack {
+                                Text("Conta Digital")
+                                
+                                Spacer()
+                                
+                                Image(systemName: "chevron.right")
+                            }
+                            .foregroundColor(.gray.opacity(0.7))
+                            
+                            Spacer()
+                            
+                            Text("Em breve uma conta completa pra você.")
+                                .foregroundColor(.black.opacity(0.7))
+                        }
+                        .padding(12)
+                        
+                    )
+            })
+            
+            Button(action: {}, label: {
+                RoundedRectangle(cornerRadius: 8)
+                    .foregroundColor(.white)
+                    .overlay(
+                        VStack(alignment: .leading) {
+                            HStack {
+                                Text("Cartão de crédito")
+                                
+                                Spacer()
+                                
+                                Image(systemName: "chevron.right")
+                            }
+                            .foregroundColor(.black.opacity(0.7))
+                            
+                            Spacer()
+                            
+                            Text("Fatura aberta")
+                                .foregroundColor(.black.opacity(0.7))
+                            
+                            Text(isVisible ? "R$ 0,00" : "R$ ••••••")
+                                .foregroundColor(.black)
+                                .font(.title2)
+                                .fontWeight(.bold)
+                            
+                            Text("Fecha em 04/06")
+                                .foregroundColor(.green)
+                        }
+                        .padding(12)
+                    )
+            })
+        }
+    }
+}
+
+struct HeaderView: View {
+    @Binding var isVisible: Bool
+    
+    var body: some View {
+        HStack {
+            Circle()
+                .frame(width: 70, height: 70)
+                .foregroundColor(.white)
+            
+            VStack(alignment: .leading) {
+                Text("olá, Raphael!")
+                    .font(.title)
+                    .fontWeight(.bold)
+                
+                Button(action: {}, label: {
+                    Text("Ver perfil")
+                        .font(.system(size: 23))
+                        .foregroundColor(.black)
+                })
+            }
+            .padding(.leading)
+            
+            Spacer()
+            
+            Button(action: {
+                isVisible.toggle()
+            }, label: {
+                Image(systemName: isVisible ? "eye.fill" : "eye.slash.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 30)
+                    .foregroundColor(.black)
+            })
+        }
+    }
+}
+
+struct BackgroundShape: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: CGPoint(x: rect.minX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.midY))
+        path.addQuadCurve(to: CGPoint(x: rect.minX, y: rect.midY), control: CGPoint(x: rect.midX, y: rect.maxY * 0.6))
+        return path
     }
 }
 
